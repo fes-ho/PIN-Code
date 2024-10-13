cb3ef6d6b3960c03e1f543cf1b64e33f2a4df018
DFERARC <dferarc@etsinf.upv.es>
2024-10-13 12:34:14 +0200

    feat: streak model created

--- api/src/models/__init__.py
+++ api/src/models/__init__.py
@@ -1,5 +1,6 @@
 from .member import Member
 from .mood import Mood
 from .types_of_mood import TypesOfMood
+from .streak import Streak
 
-__all__ = ["Member", "Mood", "TypesOfMood"]
+__all__ = ["Member", "Mood", "TypesOfMood", "Streak"]
--- api/src/models/member.py
+++ api/src/models/member.py
@@ -1,10 +1,16 @@
-from uuid import UUID, uuid4
-from sqlmodel import Field, SQLModel, Relationship
+
 from .mood import Mood
+from typing import TYPE_CHECKING, List, Optional
+from uuid import UUID, uuid4
+from sqlmodel import Field, Relationship, SQLModel
+from .streak import Streak
 
 class Member(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     username: str
     password: str
     email: str
+
     moods: list["Mood"] = Relationship(back_populates="member")
+    streaks: List["Streak"] = Relationship(back_populates="member")
+
