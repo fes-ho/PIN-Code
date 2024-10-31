adb8d918d9d44f5a3d7b602dcae514924b3ec9c0
jianeng <git@jianeng.dev>
2024-10-31 18:26:13 +0100

    fix: change instructions order to create db first and then run app

--- api/src/main.py
+++ api/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import uvicorn
 from services import create_db_and_tables, drop_all_tables, empty_db_data, logger
 from routers import router as api_router
 from scripts import populate_initial_data
@@ -8,16 +9,16 @@ import os
 logger.info("Loading the environment variables")
 load_dotenv()
 
+app = FastAPI(
+    responses={404: {"description": "Not found"}},
+)
+app.include_router(api_router)
+
 if os.getenv("CREATE_DB_AND_TABLES", "false").lower() == "true":
     logger.info("Dropping the database tables")
     drop_all_tables()
     logger.info("Creating the database and tables")
     create_db_and_tables()
-
-app = FastAPI(
-    responses={404: {"description": "Not found"}},
-)
-app.include_router(api_router)
 
 if (
     os.getenv("CREATE_DB_AND_TABLES", "false").lower() == "false"
@@ -29,3 +30,8 @@ if (
 if os.getenv("POPULATE_INITIAL_DATA", "false").lower() == "true":
     logger.info("Populating initial data")
     populate_initial_data()
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host=host, port=port)
