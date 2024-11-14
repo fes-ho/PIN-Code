a08977a68f8947206c0efbb698e80233155d5831
caarlosdiaz <132602821+caarlosdiaz@users.noreply.github.com>
2024-11-14 22:10:59 +0100

    fix: fixed import

--- api/src/schemas/mood.py
+++ api/src/schemas/mood.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from uuid import UUID
 from sqlmodel import SQLModel
-from .types_of_mood import TypeOfMood
+from .type_of_mood import TypeOfMood
 from sqlite3 import Date
 
 class MoodBase(SQLModel):
