19492f682f2aad79be6f41cc6dba39d65448dd3d
SrIzan <izan.garciag@gmail.com>
2024-11-20 13:21:39 +0100

    fix: Dependencias.

--- api/src/middleware/authorization_middleware.py
+++ api/src/middleware/authorization_middleware.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
-from services import verify_jwt
+from services import verify_jwt, logger
 from starlette.middleware.base import BaseHTTPMiddleware
 
 EXCLUDED_PATHS = ['/health', '/docs', '/redoc', '/openapi.json']
@@ -19,4 +19,5 @@ class AuhtorizationMiddleware(BaseHTTPMiddleware):
             return JSONResponse(content={"detail": exc.detail}, status_code=exc.status_code)
         
         except Exception as exc:
+            logger.error(f"Unhandler error: {str(exc)}")
             return JSONResponse(content={"detail": f"Error: {str(exc)}"}, status_code=500)
