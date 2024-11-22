9698668777c335d3f8816708ef7b323e832dc3ee
SrIzan <izan.garciag@gmail.com>
2024-11-22 13:24:19 +0100

    fix: added provider to test.

--- frontend/test/log_in/log_in_widget_test.dart
+++ frontend/test/log_in/log_in_widget_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/src/domain/member.dart';
 import 'package:frontend/src/services/member_service.dart';
+import 'package:frontend/src/states/task_list_state.dart';
 import 'package:frontend/src/views/initialpage_view.dart';
 import 'package:frontend/src/views/log_in_view.dart';
 import 'package:frontend/src/views/main_view.dart';
@@ -10,6 +11,7 @@ import 'package:frontend/src/views/splash_loading_view.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'log_in_widget_test.mocks.dart';
@@ -46,13 +48,18 @@ void main() {
       GetIt.I.registerSingleton<MemberService>(mockMemberService);
 
       // Act.
-      await tester.pumpWidget(MaterialApp(
-        home: const LogIn(),
-        routes: {
-          SplashLoadingView.routeName: (_) => const SplashLoadingView(),
-          InitialPageView.routeName: (_) => const InitialPageView()
-        },
-      ));
+      await tester.pumpWidget(
+        Provider<TaskListState> (
+          create: (context) => TaskListState(),
+          child: MaterialApp(
+            home: const LogIn(),
+            routes: {
+              SplashLoadingView.routeName: (_) => const SplashLoadingView(),
+              InitialPageView.routeName: (_) => const InitialPageView()
+            },
+          ),
+        )
+      );
 
       await tester.enterText(find.byType(TextField).first, testMail);
       await tester.enterText(find.byType(TextField).at(1), testPassword);
