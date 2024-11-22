ba3a1539cd8cfa1e29f4e0b348632d8cd4af15ad
SrIzan <izan.garciag@gmail.com>
2024-11-22 12:48:52 +0100

    feat: added notifiers

--- frontend/lib/src/states/task_list_state.dart
+++ frontend/lib/src/states/task_list_state.dart
@@ -8,4 +8,14 @@ class TaskListState extends ChangeNotifier {
   final List<Task> _tasks = [];
 
   UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
+
+  void add(Task task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void remove(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
 }
