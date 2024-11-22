a92f6c257af945d5b060149ab2d40da51d0075fc
SrIzan <izan.garciag@gmail.com>
2024-11-22 17:46:14 +0100

    feat: added memberid to tasks

--- frontend/lib/src/services/task_service.dart
+++ frontend/lib/src/services/task_service.dart
@@ -4,14 +4,17 @@ import 'package:frontend/src/domain/task.dart';
 import 'package:frontend/src/services/utils/headers/headers_factory.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
+import 'member_service.dart';
 
 class TaskService {
   late Client _client;
   late HeadersFactory _headersFactory;
+  late String _memberId;
 
   TaskService() {
     _client = GetIt.I<Client>();
     _headersFactory = GetIt.I<HeadersFactory>();
+    _memberId = GetIt.I<MemberService>().getCurrentUserId();
   }
 
   Future<Task> createTask(Task task) async {
@@ -29,21 +32,16 @@ class TaskService {
   }
 
   Future<List<Task>> getTasks() async {
-    // TODO: Replace with the actual member ID
-    const memberId = '1';
     final response = await get(
-      Uri.parse('${Config.apiUrl}/members/$memberId/tasks'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+      Uri.parse('${Config.apiUrl}/members/$_memberId/tasks'),
+      headers: await _headersFactory.getDefaultHeaders()
     );
 
     if (response.statusCode == 200) {
       final List<dynamic> taskListJson = jsonDecode(response.body) as List<dynamic>;
       return taskListJson.map((json) => Task.fromJson(json as Map<String, dynamic>)).toList();
     } else {
-      throw Exception('Failed to create task');
+      throw Exception('Failed to retrieve tasks');
     }
   }
 }
-
