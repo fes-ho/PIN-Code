357696fd672fe7e1f3a9f7ab3b7a61ef486f1f16
Jianeng <git@jianeng.dev>
2024-11-26 21:57:44 +0100

    fix: MoodBase import missing

--- api/src/schemas/__init__.py
+++ api/src/schemas/__init__.py
@@ -2,6 +2,7 @@ from .member import MemberBase
 from .activity import Activity
 from .task import TaskBase, TaskCreate, TaskDurationUpdate
 from .quest import QuestDurationUpdate
+from .mood import MoodBase
 from .type_of_mood import TypeOfMood
 
 __all__ = [
